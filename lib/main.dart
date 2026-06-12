c4580a496dfa28e26d5d321de3135a82ae37a773
niyaanzr-afk <niyaanzr@gmail.com>
2026-06-12 11:13:02 +0530

    firebase configuration completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,13 @@ import 'package:registration/forgot.dart';
 import 'package:registration/login.dart';
 import 'package:registration/signup.dart';
 import 'package:registration/welcome.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+void main()async {await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(MaterialApp(
 debugShowCheckedModeBanner: false,
 home: Welcome() ),); 
